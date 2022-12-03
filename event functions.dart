793bcb4a21e3f68f3446def2eee5c5977bb820f1
AriefYolo <76680376+AriefYolo@users.noreply.github.com>
2022-12-03 21:09:02 +0800

    Intermediate commit

--- event functions.dart
+++ event functions.dart
@@ -20,7 +20,7 @@ bool? checkeventOverlap({
             //if new event is not clashing with previously booked one return true
             return true;
           }
-        } else {
+        } /*else*/ {
           //if new event is not clashing with previously booked one return true
           return true;
         }
@@ -32,3 +32,30 @@ bool? checkeventOverlap({
 
 //function to print event details --  Arief
 
+void printDetails(List events)
+{
+  print("~~~~~~Booked Events~~~~~~");
+  //If the list is not empty, It will print all available events
+  if(events.length !=0)
+  {
+  for (int i=0;i < events.length; i++)
+  {
+    print("Event Details - Booking Number: ${i+1}000TX");
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    print("Title: ${events[i]["Event Title"]}");
+    print("Event Organizer Name: ${events[i]["Event Organizer Name"]}");
+    print("Date: ${events[i]["Event Date"]}");
+    print("Month: ${events[i]["Event Month"]}");
+    print("Year: ${events[i]["Event year"]} ");
+    print("Starting Time: ${events[i]["Event Starting Time"]} AM");
+    print("Closing Time: ${events[i]["Event Closing Time"]} PM");
+    print("Venue: ${events[i]["Event Venue"]} ");
+    print("");
+  }
+  }
+  //If the list is empty
+  else
+  {
+    print("No events available");
+  }
+}
--- main.dart
+++ main.dart
@@ -1,4 +1,4 @@
-import 'dart:html';
+//import 'dart:html';
 import 'dart:io';
 import 'event functions.dart';
 
@@ -10,6 +10,9 @@ enum Venue {
 }
 
 void main() {
+  String? eventTitle;
+  String organizerName;
+  String? continueToBook;
   int eventStartingTime = 8;
   int eventEndingTime = 5;
   int eventDate;
@@ -17,8 +20,7 @@ void main() {
   bool checkVenue;
   var currentDate = DateTime.now();
 
-// Back up Intro screen if Html doeesnt work
-//
+  // Back up Intro screen if Html doeesnt work
   print('------------------------------');
   print('-           WELCOME          -');
   print('-             TO             -');
@@ -30,10 +32,68 @@ void main() {
   var events = <Map>[]; // Storing event details
 
   // User Input -- Arief
+  do 
+  {
+    print("1. Book a new Event");
+    print("2. Check events list");
+    print("Choose (1/2) : ");
+
+  //parsing the input to integer
+    var chooseFromList = stdin.readLineSync();
+    var chooseFromListintNumber = int.parse(chooseFromList!);
+
+    if (chooseFromListintNumber == 1) 
+    {
+  
+      print("Enter event Title: ");
+      eventTitle = stdin.readLineSync();
+  
+      print("Enter Organizer Name:");
+      organizerName = stdin.readLineSync()!;
+
+      print("Select Schedule for the events (Date, Month, Year)");
+    
+    }
+  
 
   // Date validation -- Zawad
 
-  // Venue validation -- Arief
 
+
+
+  // Venue validation -- Arief
+  //display all available venue
+      Venue.values.forEach((venue) 
+      {
+        print('${venue.index + 1}. ${venue.name}');
+      });
+
+      var selectVenueintNumber;
+
+    do 
+    {
+        print("Choose (1/2): ");
+        var selectVenue = stdin.readLineSync();
+        selectVenueintNumber = int.parse(selectVenue!);
+
+        checkVenue = false;
+
+        if (selectVenueintNumber < 1 || selectVenueintNumber > 2) 
+        {
+          print('Wrong Input');
+          checkVenue = true;
+        }
+        selectVenueintNumber--;
+      } 
+      while (checkVenue == true);
+  
   // Booking validation -- Zawad
+
+  
+  
+  //ask user is he/she want to continue to end the program
+    print('Do you want to continue: (Y/N) : ');
+    continueToBook = stdin.readLineSync();
+  } 
+  while (continueToBook == 'Y' || continueToBook == 'y');
 }
